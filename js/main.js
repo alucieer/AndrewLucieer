d83cbe2bb30cd4f5c902772ef5b0cade7fe324f3
Andrew Lucieer <alucieer@gmail.com>
2015-07-08 13:30:13 -0400

    Improved menu navigation on mobile

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
 var _defaultPage = 'portfolio';
 
 var pageLoad = function() {
-	console.log('building page');
 	loadContent(_defaultPage);
 }
 
 //Asynchronously populate main tag with contents
 var loadContent = function(pageName) {
-	$('main').load(pageName + '.html', function() {
-		$('#scrim').click();
-	});
+	$('main').load(pageName + '.html');
 	
 	$('header#right').html(pageName);
-	$('nav a').css('font-weight', 'normal');
-	$('a#' + pageName).css('font-weight', 'bold');
+	$('nav .nav-item').css('background', 'white');
+	$('nav #' + pageName).css('background', '#eceff1');
+	
+	return true;
 }
+
